2381cf3bdb439d350715c02d3c9793104534d3dc
muskan <muskan1802agrawal@gmail.com>
2021-10-31 22:40:24 +0530

    web cam enabled

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -13,9 +13,44 @@ for cls in mylist:
     classNames.append(os.path.splitext(cls)[0])
 print(classNames)
 
+def findencoding(images):
+    encodelist =[]
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodelist.append(encode)
+    return encodelist
 
+encodelistknown = findencoding(images)
+print('Encoding Complete')
 
-# imgElon = face_recognition.load_image_file('ImagesBasic/elonmusk.jpg')
-# imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
-# imgTest = face_recognition.load_image_file('ImagesBasic/elonmusktest.jpg')
-# imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facescurframe = face_recognition.face_locations(imgS)
+    encodecurframe = face_recognition.face_encodings(imgS,facescurframe)
+
+    for encodeface,faceloc in zip(encodecurframe,facescurframe):
+        matches = face_recognition.compare_faces(encodelistknown,encodeface)
+        facedis = face_recognition.face_distance(encodelistknown,encodeface)
+        print(facedis)
+        matcheindex = np.argmin(facedis)
+
+        if matches[matcheindex]:
+            name = classNames[matcheindex].upper()
+            print(name)
+
+# faceLoc = face_recognition.face_locations(imgElon)[0]
+# encodeElon = face_recognition.face_encodings(imgElon)[0]
+# cv2.rectangle(imgElon,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255),2)
+#
+# faceLocTest = face_recognition.face_locations(imgTest)[0]
+# encodeTest = face_recognition.face_encodings(imgTest)[0]
+# cv2.rectangle(imgTest,(faceLocTest[3],faceLocTest[0]),(faceLocTest[1],faceLocTest[2]),(255,0,255),2)
+#
+# results = face_recognition.compare_faces([encodeElon],encodeTest)
+# faceDis = face_recognition.face_distance([encodeElon],encodeTest)
